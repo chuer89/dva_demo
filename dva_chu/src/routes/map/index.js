e6e5f022d1f3bf52f6e0e5128666b6d7411e18d0
liangchu <chuer89@gmail.com>
2018-05-24 17:00:31 +0800

    up

--- dva_chu/src/routes/map/index.js
+++ dva_chu/src/routes/map/index.js
@@ -1,16 +1,34 @@
-import {connect} from 'dva';
+import { connect } from 'dva';
+import React from 'react';
 import { Button } from 'antd';
 import { Link } from 'dva/router';
 import style from './index.less';
+import { Map } from 'react-amap';
 
-console.log(style);
+class MapList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amapkey: '30678ae375a00a1107177e2a272d1ec9',
+      amapVersion: '1.4.6'
+    };
 
-const Map = () => {
-  return (
-    <div>
-      22
-    </div>
-  )
+    this.mapCenter = { longitude: 120, latitude: 30 };
+  }
+
+  render() {
+    var pos = { longitude: 120, latitude: 30 };
+
+    return (
+      <div style={{ width: 600, height: 400 }}>
+        <h1>地图</h1>
+        <Map zoom={5} 
+          center={this.mapCenter} 
+          amapkey={this.state.amapkey} 
+          version={this.state.amapVersion} />
+      </div>
+    )
+  }
 }
 
-export default connect()(Map);
+export default connect()(MapList);
